160363e0c7560456f7af9e3e5f8f86900804ca7f
abdullahbhattidev <ab76293@gmail.com>
2024-02-02 15:53:59 +0500

    chakra ui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
   
   return (
-    <button>hiiiiii</button>
+    <Button colorScheme='blue'>Button</Button>
   )
 }
 
