51525c023444a08d6ba676bd6fb24238b37119f9
abdullahbhattidev <ab76293@gmail.com>
2024-02-08 02:22:24 +0500

    optimize card images

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 import {Game} from "../hooks/useGames"
 import PlatformIconList from './PlatformIconList'
 import Metacritic from './Metacritic'
+import cropImageUrl from '../cropImage'
 interface props {
     game: Game
 }
 const GameCards = ({game}: props) => {
   return (
     <Card borderRadius={10} overflow="hidden" boxShadow="0 3px 10px rgb(0 0 0 / 0.2)">
-     <Image src={game.background_image}/>
+     <Image src={cropImageUrl(game.background_image) }/>
      <CardBody>
       <Heading fontSize="2xlg">{game.name}</Heading>
       <HStack justifyContent="space-between">
