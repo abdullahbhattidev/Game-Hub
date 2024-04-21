556c3d93626d4c8471641f9c8eef53cb6facceba
abdullahbhattidev <ab76293@gmail.com>
2024-04-21 18:09:58 +0500

    handle route errors

--- src/Route.tsx
+++ src/Route.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import GameDetail from "./pages/GameDetail";
+import ErrorPage from "./pages/ErrorPage";
 
 const route = createBrowserRouter([
     {path:"/",
      element:<Layout/>,
+     errorElement: <ErrorPage/>,
      children:[
         {index: true, element:<HomePage/>},
         {path: "gamedetails/:id", element:<GameDetail/>}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,15 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchBar from "./SearchBar";
+import { useNavigate } from "react-router-dom";
 
 
 
 function NavBar (){
+    const navigate  = useNavigate()
     return (
         <HStack justifyContent="space-between" padding="10px">
-            <Image src={logo} boxSize="60px"/>
+            <Image src={logo} boxSize="60px" onClick={()=> navigate("/") }/>
             <SearchBar />
             <ColorModeSwitch/>
         </HStack>
